8a24f6ada963a60757670ab5b82a49715a9daece
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-06-10 17:13:31 -0400

    adding new codes

---
+++ aula4/index.js
@@ -0,0 +1,17 @@
+// comentário
+console.log("Olá mundo!"); // outro comentário
+console.log("linha 1");
+// comentário
+
+/* 
+
+oiiii
+oiiii
+oiiii
+
+*/
+
+console.log("linha 2");
+console.log("linha 3");
+console.log("linha 4");
+// esse comentário não será lido

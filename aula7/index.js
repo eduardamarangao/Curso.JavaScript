e312492730323dbe52e51d936e8330596903d5bc
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-06-11 14:26:35 -0400

    adding new codes

--- aula7/index.js
+++ aula7/index.js
@@ -2,5 +2,5 @@
 const primeiroNumero = 6;
 const segundoNumero = 7;
 const resultado = primeiroNumero * segundoNumero;
-//const resultaDuplicado = resultado * 2;
+const resultaDuplicado = resultado * 2;
 console.log(resultaDuplicado);

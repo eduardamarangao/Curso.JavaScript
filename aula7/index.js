82ab1b91c3b0eedeb33e911cfc4922089dc13399
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-06-13 16:54:42 -0400

    modificando arquivo index

--- aula7/index.js
+++ aula7/index.js
@@ -1,3 +1,9 @@
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// Não pode começar o nome de uma constante com um número
+// Utilizamos camelCase
+// Case-sensitive
+// Não pode modificar o valor de uma constante
 // String = Text | Number = Número
 const primeiroNumero = 6;
 const segundoNumero = 7;

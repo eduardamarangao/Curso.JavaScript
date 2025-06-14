441509e87f7eebaf5bb9fd14f7bbd250f2f7e6c2
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-06-14 16:45:18 -0400

    modificando arquivo index

--- aula10/index.js
+++ aula10/index.js
@@ -1,6 +1,9 @@
-// NaN - Not a number parseInt (inteiro), parseFloat(decimais)
-const num1 = 10;
-const num2 = Number('Luiz');
+/* 
+* Aritméticos
+* + Adição / Concatenação
+* - / *
+*/
 
-console.log(num1 + num2);
-console.log(typeof num2);
+const num1 = 5
+const num2 = 10
+console.log(num1 + num2)

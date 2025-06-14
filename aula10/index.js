c69bb2d6c11063193d273d98b4372f21d9567fd5
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-06-14 16:50:36 -0400

    modificando arquivo index

--- aula10/index.js
+++ aula10/index.js
@@ -1,9 +1,11 @@
 /* 
 * Aritméticos
-* + Adição / Concatenação
-* - / *
++ Adição / Concatenação ( - / *)
+** Potenciação
+% Resto da divisão
 */
 
-const num1 = 5
-const num2 = 10
-console.log(num1 + num2)
+const num1 = 10;
+const num2 = 5;
+const num3 = 10;
+console.log((num1 + num2) * num3);

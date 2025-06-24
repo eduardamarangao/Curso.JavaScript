4180def65c6506007fd9601f45c570a3e5f5311a
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-06-24 19:12:11 -0400

    adding new codes

--- aula11/js/script.js
+++ aula11/js/script.js
@@ -1,3 +1,7 @@
-var nome = window.prompt('Digite seu nome: ')
-alert(`Olá, ${nome}`)
-//window.alert()
+let num1 = prompt('Digite um número:');
+let num2 = prompt('Digite outro número');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+alert(`O resultado da sua conta foi: ${num1 + num2}`);

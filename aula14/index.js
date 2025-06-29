f28278cc21f8f7f35e7fe6eb6fc8982e7cf13a13
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-06-29 15:22:57 -0400

    adding new codes

---
+++ aula14/index.js
@@ -0,0 +1,7 @@
+let num1 = 1;
+let num2 = 3.5;
+
+/*
+console.log(num1.toString() + num2)
+console.log(typeof num1)
+*/
--- variavel.js
+++
@@ -1,8 +0,0 @@
-/**
- * let: Permite criar variáveis com escopo de bloco.
- * Exemplo: let x = 1;
- *
- * var: Cria variáveis com escopo de função.
- * Exemplo: var y = 2;
- */
-

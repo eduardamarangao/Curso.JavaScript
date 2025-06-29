a8da71c6177d5cda17e445cf52919776c73ee293
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-06-29 15:25:35 -0400

    adding new codes

--- aula14/index.js
+++ aula14/index.js
@@ -1,8 +1,8 @@
-let num1 = 10;
+let num1 = 10.3237379323;
 let num2 = 3.5;
 
 /*
 console.log(num1.toString() + num2)
 console.log(typeof num1)
 */
-console.log(num1 = num1.toString(2))
+console.log(num1.toFixed(2))

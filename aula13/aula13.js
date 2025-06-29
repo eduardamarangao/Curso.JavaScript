3a83983d4f8e804ade004efcfd1e768af42e09cc
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-06-29 15:13:40 -0400

    modificando código

--- aula13/aula13.js
+++ aula13/aula13.js
@@ -1,7 +1,4 @@
 //               012345
 let umaString = "O rato roeu a roupa do rei de roma.";
 
-console.log(umaString.slice(-5, umaString.length -1))
-console.log(umaString.substring(umaString.length -5, umaString.length -1))
-
-//console.log(umaString.slice(2, 6))
+console.log(umaString.split(' ', 4))

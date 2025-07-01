277199524bf9fb73d0bdc1c4e3adc145a3a88892
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-07-01 15:47:04 -0400

    adding new codes

--- aula14/index.js
+++ aula14/index.js
@@ -1,10 +1,18 @@
-let num1 = 10;
-let num2 = 3.5;
+// IEEE 754 - 2008
+let num1 = 0.7; // number
+let num2 = 0.1; // number
+ 
+num1 += num2 // num1 = num2 + num2
+num1 += num2
+num1 += num2
 
+num1 = num1.toFixed(2)
+ 
+console.log(num1)
 
 //console.log(num1.toString() + num2)
 //console.log(typeof num1)
 //console.log(num1.toFixed(2))
 //console.log(Number.isInteger(num1))
-let temp = num1 * '5'
-console.log(Number.isNaN(temp))
+//let temp = num1 * '5'
+//console.log(temp)

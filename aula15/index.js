de2f6764e77daed7ebf21ea01e81abd3552aebb9
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-07-01 16:19:16 -0400

    adding new codes

--- aula15/index.js
+++ aula15/index.js
@@ -1,3 +1,10 @@
-let num1 = 9.452323
-let num2 = Math.ceil(num1)
-console.log(num2)
+let num1 = 100
+console.log(num1 ** 0.5) //(1/2))
+//let num2 = Math.floor(num1)
+//let num2 = Math.ceil(num1)
+//let num2 = Math.round(num1)
+//console.log(Math.max(1,2,-2,56,-76,32,3432,9999,432.4,-99))
+//console.log(Math.min(1,2,-2,56,-76,32,3432,9999,432.4,-99))
+//const aleatorio = Math.round(Math.random()) * (9999 - 1) + 1
+//console.log(aleatorio)
+//console.log(Math.pow(2,10)) console.log(2 ** 10)

a7f82ba4eeefb49ca8f14f4b82e1b0848e450818
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-07-01 16:38:13 -0400

    adding new codes

---
+++ aula15/ex/script.js
@@ -0,0 +1,6 @@
+const numero = prompt("Digite um número: ");
+const NumeroTitulo = document.querySelector("#numero-titulo");
+const Texto = document.getElementById("texto");
+
+NumeroTitulo.innerHTML = numero;
+Texto.innerHTML = `<p>Seu número - 2 é ${numero - 2}.</p>`

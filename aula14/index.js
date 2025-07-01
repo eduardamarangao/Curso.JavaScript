31f089bc1b60a20c843da8d43c37d210d79839ba
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-07-01 15:55:14 -0400

    adding new codes

--- aula14/index.js
+++ aula14/index.js
@@ -2,17 +2,11 @@
 let num1 = 0.7; // number
 let num2 = 0.1; // number
  
-num1 += num2 // 0.8
-num1 += num2 // 0.9 
-num1 += num2 // 1.0
-num1 += num2 // 1.1
-num1 += num2 // 1.2
-num1 += num2 // 1.3
-num1 += num2 // 1.4
-num1 += num2 // 1.5
+num1 = ((num1 * 100) + (num2* 100)) / 100 // 0.8
+num1 = ((num1 * 100) + (num2* 100)) / 100 // 0.9
+num1 = ((num1 * 100) + (num2* 100)) / 100 // 1.0
 
-num1 = parseFloat(num1.toFixed(2))
- 
+
 console.log(num1)
 console.log(Number.isInteger(num1))
 

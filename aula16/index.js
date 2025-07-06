513fa39631fea395a25b6e051e3c9287d3ef2673
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-07-06 13:57:15 -0400

    adding codes

--- aula16/index.js
+++ aula16/index.js
@@ -1,7 +1,13 @@
 const alunos = ["duda", "maria", "joao"];
 
+alunos.push('luiza')
+alunos.push('fabio')
+
+/*
 alunos[alunos.length] = 'luis'
 alunos[alunos.length] = 'fábio'
 alunos[alunos.length] = 'julia'
+*/
 console.log(alunos)
+
 

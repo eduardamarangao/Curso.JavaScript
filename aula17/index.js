a01b0d703cb0027aed5d285286854ce62896738f
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-07-09 16:07:45 -0400

    adding new codes

--- aula17/index.js
+++ aula17/index.js
@@ -1,7 +1,14 @@
-function soma(x=1, y=1){
-    const resultado = x + y
-    return resultado
+const raiz = function (n) {
+    return `A raiz de ${n} é ${n ** 0.5}`
+};     
+
+console.log(raiz(9))
+
+/*
+function soma(x = 1, y = 1) {
+  const resultado = x + y;
+  return resultado;
 }
 
-const resultado = soma()
-console.log(resultado)
+const resultado = soma(3,6);
+console.log(resultado);*/

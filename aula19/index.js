08777fa7738e70826663b4af72ee748ad88d1279
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-07-12 17:46:39 -0400

    adding codes

--- aula19/index.js
+++ aula19/index.js
@@ -2,7 +2,9 @@
 Primitivos (imutáveis) - string, number, boolean, undefined,
 null (bigint, symbol)
 */
-//          0123456
-let nome = 'eduarda'
-nome[6] = 'o'
-console.log(nome[6], nome) // imutável
+let a = 'A' 
+let b = a // cópia 
+console.log(a,b)
+
+a = 'Outra coisa'
+console.log(a,b)

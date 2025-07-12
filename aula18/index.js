9f801b0375dea3be61995f9d2106904004c8d567
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-07-12 17:29:05 -0400

    adding codes

--- aula18/index.js
+++ aula18/index.js
@@ -1,12 +1,49 @@
-function criaPessoa(nome, sobrenome, idade) {
-  return { nome, sobrenome, idade }; // nome: nome
+const pessoa1 = {
+    nome: 'eduarda',
+    sobrenome: 'amorim',
+    idade: 15,
+
+    fala() {
+        console.log('olá mundo!')
+    }
 }
 
-const pessoa1 = criaPessoa("luiz", "otávio", 25);
-const pessoa2 = criaPessoa("eduarda", "amorim", 15);
-const pessoa3 = criaPessoa("ilza", "alves", 39);
-const pessoa4 = criaPessoa("alisson", "marangão", 43);
-const pessoa5 = criaPessoa("julia", "silva", 67);
-const pessoa6 = criaPessoa("maria", "de jesus", 54);
+pessoa1.fala()
 
-console.log(pessoa1.nome, pessoa2.nome);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function criaPessoa(nome, sobrenome, idade) {
+// return { nome, sobrenome, idade }; // nome: nome
+// }
+
+// const pessoa1 = criaPessoa("luiz", "otávio", 25);
+// const pessoa2 = criaPessoa("eduarda", "amorim", 15);
+// const pessoa3 = criaPessoa("ilza", "alves", 39);
+// const pessoa4 = criaPessoa("alisson", "marangão", 43);
+// const pessoa5 = criaPessoa("julia", "silva", 67);
+// const pessoa6 = criaPessoa("maria", "de jesus", 54);
+
+// console.log(pessoa1.nome, pessoa2.nome);

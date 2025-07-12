adcf36fbaf9b7b59cdef23b4d78c363cd5a37c0c
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-07-12 17:24:00 -0400

    adding codes

--- aula18/index.js
+++ aula18/index.js
@@ -1,16 +1,12 @@
-function criaPessoa (nome, sobrenome, idade) {
-    return {
-        nome: nome, 
-        sobrenome: sobrenome,
-        idade: idade
-    }
+function criaPessoa(nome, sobrenome, idade) {
+  return { nome, sobrenome, idade }; // nome: nome
 }
 
-const pessoa1 = criaPessoa('luiz', 'otávio', 25)
-const pessoa2 = criaPessoa('eduarda', 'amorim', 15)
-const pessoa3 = criaPessoa('ilza', 'alves', 39)
-const pessoa4 = criaPessoa('alisson', 'marangão', 43)
-const pessoa5 = criaPessoa('julia', 'silva', 67)
-const pessoa6 = criaPessoa('maria', 'de jesus', 54)
+const pessoa1 = criaPessoa("luiz", "otávio", 25);
+const pessoa2 = criaPessoa("eduarda", "amorim", 15);
+const pessoa3 = criaPessoa("ilza", "alves", 39);
+const pessoa4 = criaPessoa("alisson", "marangão", 43);
+const pessoa5 = criaPessoa("julia", "silva", 67);
+const pessoa6 = criaPessoa("maria", "de jesus", 54);
 
-
+console.log(pessoa1.nome, pessoa2.nome);

0e7142621625c2222ee16a92a7b52fd46ca66576
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-07-20 15:56:54 -0400

    adding new codes

--- aula19/index.js
+++ aula19/index.js
@@ -1,8 +1,8 @@
 /*
 Primitivos (imutáveis) - string, number, boolean, undefined,
-null (bigint, symbol)
+null (bigint, symbol) - Valores copiados
 
-Referência (mutável) - Array, object, function
+Referência (mutável) - Array, object, function - Passados por referência
 */
 
 let a = [1,2,3] // os dois apontam para o mesmo lugar na memória
@@ -11,3 +11,6 @@ console.log(a,b)
 
 a.push(4)
 console.log(a,b)
+
+b.pop()
+console.log(a, b)

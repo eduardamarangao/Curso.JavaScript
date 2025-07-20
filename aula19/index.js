60c398660ff96b47b19e4d5c7b299efdd9744d56
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-07-20 15:54:51 -0400

    adding new codes

--- aula19/index.js
+++ aula19/index.js
@@ -5,5 +5,9 @@ null (bigint, symbol)
 Referência (mutável) - Array, object, function
 */
 
-let a = [1,2,3]
+let a = [1,2,3] // os dois apontam para o mesmo lugar na memória
 let b = a
+console.log(a,b)
+
+a.push(4)
+console.log(a,b)

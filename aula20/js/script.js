9d084072dc08c80e6e1ad53db9740addc1840cd6
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-07-26 19:15:24 -0400

    adding new codes

--- aula20/js/script.js
+++ aula20/js/script.js
@@ -2,7 +2,9 @@ function meuEscopo () {
     const form = document.querySelector('.form')
 
     form.onsubmit = function (evento) {
+        evento.preventDefaul() // previne oque acontece por padrao
         alert(1)
+        console.log('Enviado')
     };
 }
 

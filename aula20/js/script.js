7909c4de62023b804cfc63fae6ba24ae18e2e6b7
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-07-29 20:39:38 -0400

    adding new codes

--- aula20/js/script.js
+++ aula20/js/script.js
@@ -1,11 +1,20 @@
 function meuEscopo () {
     const form = document.querySelector('.form')
 
+    /*
     form.onsubmit = function (evento) {
         evento.preventDefaul() // previne oque acontece por padrao
         alert(1)
         console.log('Enviado')
     };
+    */
+
+    function recebeEventoForm (evento) {
+        evento.preventDefault()
+        console.log('Form não foi enviado')
+    }
+
+    form.addEventListener('submit', recebeEventoForm)
 }
 
 meuEscopo()

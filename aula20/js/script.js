98fa82a73803bfc9bb991807d85034f5e68d48a1
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-08-01 21:53:04 -0400

    adding codes

--- aula20/js/script.js
+++ aula20/js/script.js
@@ -1,30 +1,31 @@
-function meuEscopo () {
-  const form = document.querySelector('.form');
-  const resultado = document.querySelector('.resultado');
+function meuEscopo() {
+  const form = document.querySelector(".form");
+  const resultado = document.querySelector(".resultado");
 
   const pessoas = [];
 
-  function recebeEventoForm (evento) {
+  function recebeEventoForm(evento) {
     evento.preventDefault();
 
-    const nome = form.querySelector('.nome');
-    const sobrenome = form.querySelector('.sobrenome');
-    const peso = form.querySelector('.peso');
-    const altura = form.querySelector('.altura');
+    const nome = form.querySelector(".nome");
+    const sobrenome = form.querySelector(".sobrenome");
+    const peso = form.querySelector(".peso");
+    const altura = form.querySelector(".altura");
 
     pessoas.push({
       nome: nome.value,
       sobrenome: sobrenome.value,
       peso: peso.value,
-      altura: altura.value
+      altura: altura.value,
     });
 
     console.log(pessoas);
 
-    resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ` +
+    resultado.innerHTML +=
+      `<p>${nome.value} ${sobrenome.value} ` +
       `${peso.value} ${altura.value}</p>`;
   }
 
-  form.addEventListener('submit', recebeEventoForm);
+  form.addEventListener("submit", recebeEventoForm);
 }
 meuEscopo();

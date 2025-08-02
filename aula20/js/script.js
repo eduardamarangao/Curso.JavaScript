b40d91e3213e308dbb66e40af0016fa3c7883af2
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-08-01 21:52:25 -0400

    adding codes

--- aula20/js/script.js
+++ aula20/js/script.js
@@ -1,31 +1,30 @@
 function meuEscopo () {
-    const form = document.querySelector('.form')
-    const resultado = document.querySelector('.resultado')
+  const form = document.querySelector('.form');
+  const resultado = document.querySelector('.resultado');
 
-    const pessoas = []
+  const pessoas = [];
 
-    function recebeEventoForm (evento) {
-        evento.preventDefault()
+  function recebeEventoForm (evento) {
+    evento.preventDefault();
 
-        const nome = form.querySelector('.nome')
-        const sobrenome = form.querySelector('.sobrenome')
-        const peso = form.querySelector('.peso')
-        const altura = form.querySelector('.altura')
+    const nome = form.querySelector('.nome');
+    const sobrenome = form.querySelector('.sobrenome');
+    const peso = form.querySelector('.peso');
+    const altura = form.querySelector('.altura');
 
-        pessoas.push({
-            nome: nome.value,
-            sobrenome: sobrenome.value,
-            peso: peso.value,
-            altura: altura.value
-        });
+    pessoas.push({
+      nome: nome.value,
+      sobrenome: sobrenome.value,
+      peso: peso.value,
+      altura: altura.value
+    });
 
-        console.log(pessoas)
+    console.log(pessoas);
 
-        resultado.innerHTML += `<p>nome: ${nome.value}. sobrenome: ${sobrenome.value}.</p>` +
-        `peso: ${peso.value}. altura:${altura.value}.`
-    }
+    resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ` +
+      `${peso.value} ${altura.value}</p>`;
+  }
 
-    form.addEventListener('submit', recebeEventoForm)
+  form.addEventListener('submit', recebeEventoForm);
 }
-
-meuEscopo()
+meuEscopo();

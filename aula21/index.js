6bed4c365840b168be8127f54043b798df07ade5
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-08-04 19:57:31 -0400

    adding new codes

--- aula21/index.js
+++ aula21/index.js
@@ -6,8 +6,10 @@ Operadores de comparaçao
 <= menor que ou igual a 
 == igualdade (valor) ************
 === igualdade estrita (valor e tipo)
-!= diferente (valor) **********
+!= diferente (valor) ************
 !== diferente estrio (valor e tipo)
 */
-const comp = 10 > 5
+const num1 = 10
+const num2 = '10'
+const comp = num1 == num2
 console.log(comp)

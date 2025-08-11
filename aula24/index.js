872820477df7831eb29f49e53be7606f97b268ca
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-08-11 17:44:02 -0400

    adding new codes

--- aula24/index.js
+++ aula24/index.js
@@ -1,7 +1,12 @@
-const hora = 10
+/*
+Entre 0 - 11 -> Bom dia
+Entre 12 - 17 -> Boa tarde
+Entre 18 - 23 -> Boa noite
+*/
+
+const hora = 13
 
 if (hora <= 12) { // se for verdadeiro
     console.log('Bom dia')
 
-    console.log(1 + 1)
 }

7c8af7195ce8b7b9c3daba2ad7be4ff887139336
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-08-14 15:43:25 -0400

    adding codes

--- aula25/index.js
+++ aula25/index.js
@@ -1,17 +1,20 @@
-const numero = 9;
+const numero = 10;
 
 // Se isso (numero >= 0 && numero <= 5) ocorrer, faça isso {código}
 // Se não, faça isso {código}
+
+if (numero <= 10) {
+    console.log(`${numero} é menor ou igual a 10.`)
+}
 
 if (numero >= 0 && numero <= 5) {
   console.log(`${numero} está entre 0 e 5.`);
 } else if (numero >= 6 && numero <= 8) {
   console.log(`${numero} está entre 6 e 8.`);
-}  else if (numero >= 9 && numero <= 11) {
+} else if (numero >= 9 && numero <= 11) {
   console.log(`${numero} está entre 9 e 11.`);
 } else {
   console.log(`${numero} nao está entre 0 e 11.`);
 }
 
-
-console.log('Aqui vem o resto do código...')
+console.log("Aqui vai o resto do código...");

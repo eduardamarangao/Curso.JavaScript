16fa3a59a185d8ba7079ce01156da21af41cf0d7
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-08-14 15:33:17 -0400

    adding codes

--- aula25/index.js
+++ aula25/index.js
@@ -1,10 +1,14 @@
 const numero = 8
 
+// Se isso (numero >= 0 && numero <= 5) ocorrer, faça isso {código}
+// Se não, faça isso {código}
 
 if (numero >= 0 && numero <= 5) {
     console.log(`${numero} está entre 0 e 5.`)
-} //else {
-   //console.log(`${numero} NÃO está entre 0 e 5.`)
-//}
-
-console.log('O numero nao está entre 0 e 5.') 
+} else if (numero >= 6 && numero <= 8) {
+    console.log(`${numero} está entre 6 e 8.`)
+} else if (numero >= 9 && numero <= 11) {
+    console.log(`${numero} está entre 9 e 11.`)
+} else {
+    console.log(`${numero} nao está entre 0 e 11.`)
+}

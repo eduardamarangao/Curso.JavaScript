3b2ed8c633fcbd236b208918856c4db6d162a2f0
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-08-14 15:27:45 -0400

    adding codes

--- aula25/index.js
+++ aula25/index.js
@@ -1,6 +1,10 @@
-const numero = 3
+const numero = 8
 
 
 if (numero >= 0 && numero <= 5) {
     console.log(`${numero} está entre 0 e 5.`)
-} 
+} //else {
+   //console.log(`${numero} NÃO está entre 0 e 5.`)
+//}
+
+console.log('O numero nao está entre 0 e 5.') 

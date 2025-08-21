7ca2ca01a79877bdc7b20a3a689da3c80af8cf28
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-08-21 16:25:24 -0400

    adding new codes

--- aula26/main.js
+++ aula26/main.js
@@ -1,7 +1,16 @@
+const { set } = require("mongoose");
+
 // Capturar evento de submit do formulário
 const form = document.querySelector("#form");
 
 form.addEventListener("submit", function (evento) {
     evento.preventDefault()
     console.log('Evento prevenido')
+    setResultado('Olá mundo!')
 });
+
+
+function setResultado(msg) {
+    const resultado = document.querySelector('#resultado')
+    resultado.innerHTML = msg;
+}

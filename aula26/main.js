1e9b830ef8090c7209e08215d5a7e750dd63d860
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-08-21 16:20:51 -0400

    adding new codes

--- aula26/main.js
+++ aula26/main.js
@@ -1,6 +1,7 @@
 // Capturar evento de submit do formulário
 const form = document.querySelector("#form");
 
-form.addEventListener("submit", function () {
-    
+form.addEventListener("submit", function (evento) {
+    evento.preventDefault()
+    console.log('Evento prevenido')
 });

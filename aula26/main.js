10492e68fa73c99652b5f15a08caaac8b5c6f508
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-08-23 14:35:13 -0400

    adding codes

--- aula26/main.js
+++ aula26/main.js
@@ -11,4 +11,6 @@ form.addEventListener("submit", function (e) {
 function setResultado(msg) {
     const resultado = document.querySelector('#resultado')
     resultado.innerHTML = "";
+    const p = document.createElement('p')
+    p.innerHTML = 'Qualquer coisa'
 }

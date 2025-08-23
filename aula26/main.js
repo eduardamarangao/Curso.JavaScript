ebc11db632b563b4d9158efc6c696c375117b645
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-08-23 14:49:25 -0400

    adding codes

--- aula26/main.js
+++ aula26/main.js
@@ -3,16 +3,17 @@ const form = document.querySelector("#form");
 
 form.addEventListener("submit", function (e) {
     e.preventDefault()
-    console.log('Evento prevenido')
-    setResultado('Olá mundo!')
 });
+
+function criaP () {
+    const p = document.createElement('p')
+    return p
+}
 
 
 function setResultado(msg) {
     const resultado = document.querySelector('#resultado')
     resultado.innerHTML = "";
-    const p = document.createElement('p')
-    p.classList.add('paragrafo-resultado')
-    p.innerHTML = 'Qualquer coisa'
-    resultado.appendChild(p)
+
+    const p = criaP()
 }
